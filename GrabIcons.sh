7da17444fa52ae2e7c13933cd96fe8549504dd33
NolanSyKinsley <NolanSyKinsley@gmail.com>
2015-04-15 08:56:01 -0700

    Update GrabIcons.sh
    Cleanup vestigial code.

--- GrabIcons.sh
+++ GrabIcons.sh
@@ -7,7 +7,6 @@ KEY=
 
 for file in $(ls $STEAMAPPS/*.acf -1v); do
     ID=$(cat "$file" | grep '"appid"' | head -1 | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/')
-    NAME=$(cat "$file" | grep '"name"' | head -1 | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/')
     ICONHASH=$(curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$KEY&steamid=$STEAMID&include_appinfo=1&format=json" | jq '.response.games[] | select(.appid == '$ID') | .img_icon_url' | sed 's/^"\(.*\)"$/\1/') 
 wget http://media.steampowered.com/steamcommunity/public/images/apps/$ID/$ICONHASH.jpg -O $ICONS/steam_icon_$ID.png
 done
