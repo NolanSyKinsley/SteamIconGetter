a494eb2c6fade29e73714fb85340d0b833a08ca9
NolanSyKinsley <NolanSyKinsley@gmail.com>
2015-04-15 22:06:34 -0700

    Update GrabIcons.sh
    cleanup/standardize code.

--- GrabIcons.sh
+++ GrabIcons.sh
@@ -4,8 +4,7 @@ STEAMAPPS=~/.steam/steam/steamapps/
 ICONS=~/.local/share/icons/hicolor/32x32/apps/
 
 for file in $(ls $STEAMAPPS/*.acf -1v); do
-    ID=$(cat "$file" | grep '"appid"' | head -1 | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/')
-    NAME=$(cat "$file" | grep '"name"' | head -1 | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/')
+    ID=$(cat "$file" | grep -w -m 1 "appid" | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/')
     ICONHASH=$(steamcmd  +app_info_print $ID +exit | grep -w -m 1 "icon" | sed -r 's/[^"]*"icon"[^"]*"([^"]*)"/\1/') 
 wget http://media.steampowered.com/steamcommunity/public/images/apps/$ID/$ICONHASH.jpg -O $ICONS/steam_icon_$ID.png
 done
