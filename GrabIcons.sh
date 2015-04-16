8137dc3f8e945d8cd3d617ea60cd0d9443f11f57
NolanSyKinsley <NolanSyKinsley@gmail.com>
2015-04-15 18:42:13 -0700

    Remove need for Steam64 ID and Web API Keys
    Now Requires SteamCMD

--- GrabIcons.sh
+++ GrabIcons.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-STEAMID=
 STEAMAPPS=~/.steam/steam/steamapps/
 ICONS=~/.local/share/icons/hicolor/32x32/apps/
-KEY=
 
 for file in $(ls $STEAMAPPS/*.acf -1v); do
     ID=$(cat "$file" | grep '"appid"' | head -1 | sed -r 's/[^"]*"appid"[^"]*"([^"]*)"/\1/')
-    ICONHASH=$(curl "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$KEY&steamid=$STEAMID&include_appinfo=1&format=json" | jq '.response.games[] | select(.appid == '$ID') | .img_icon_url' | sed 's/^"\(.*\)"$/\1/') 
+    NAME=$(cat "$file" | grep '"name"' | head -1 | sed -r 's/[^"]*"name"[^"]*"([^"]*)"/\1/')
+    ICONHASH=$(steamcmd  +app_info_print $ID +exit | grep -w -m 1 "icon" | sed -r 's/[^"]*"icon"[^"]*"([^"]*)"/\1/') 
 wget http://media.steampowered.com/steamcommunity/public/images/apps/$ID/$ICONHASH.jpg -O $ICONS/steam_icon_$ID.png
 done
